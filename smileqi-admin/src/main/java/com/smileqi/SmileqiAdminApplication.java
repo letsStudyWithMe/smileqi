a327d6f7c2973e3603c671699cd3509cd5ede88c
ss <1029515726@qq.com>
2023-12-18 17:07:52 +0800

    first commit：initialization

---
+++ smileqi-admin/src/main/java/com/smileqi/SmileqiAdminApplication.java
@@ -0,0 +1,13 @@
+package com.smileqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileqiAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmileqiAdminApplication.class, args);
+    }
+
+}
